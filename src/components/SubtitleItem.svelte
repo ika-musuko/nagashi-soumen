b127999fa69fedeb927a13dec38ad61bf0726ae7
sherwyn <sherwyn.sen@gmail.com>
2023-09-18 23:05:25 +0900

    B-2 - implement active subtitle coloring with new subtitle handler
    therefore, active sub no longer gets deselected on click outside of viewer

---
+++ src/components/SubtitleItem.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import { timeDisplay } from "../utils/utils";
+  export let startTime: number;
+  export let text: string;
+  export let active: boolean = false;
+
+  const dispatch = createEventDispatcher();
+  function handleClick() {
+    dispatch("click");
+  }
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<li
+  class="subtitle {active ? 'active-subtitle' : 'inactive-subtitle'}"
+  tabindex="-1"
+  on:click={handleClick}
+>
+  <span style="user-select: none; font-size: small;">
+    {timeDisplay(startTime)}:&nbsp;
+  </span>
+  <span
+    class="subtitle-text {active
+      ? 'active-subtitle-text'
+      : 'inactive-subtitle-text'}"
+  >
+    {text}
+  </span>
+</li>
+
+<style>
+  .subtitle {
+    cursor: pointer;
+    border-radius: 3px;
+  }
+
+  .inactive-subtitle:hover {
+    background-color: #333333;
+  }
+
+  .active-subtitle {
+    background-color: #773333;
+  }
+
+  .active-subtitle-text {
+    font-weight: bold;
+  }
+</style>
--- src/components/SubtitleViewer.svelte
+++ src/components/SubtitleViewer.svelte
@@ -2,35 +2,37 @@
   import { createEventDispatcher } from "svelte";
   import { timeDisplay } from "../utils/utils";
   import type { VTTCueMap } from "../subtitles/VTTCueMap";
+  import SubtitleItem from "./SubtitleItem.svelte";
 
   export let cues: VTTCueMap;
   export let activeCueIds: Set<string>;
+  $: activeCueIds, makeActiveCuesVisible();
+  function makeActiveCuesVisible() {
+    const activeSubs: HTMLCollection =
+      document.getElementsByClassName("active-subtitle");
+    if (activeSubs.length < 1) return;
+    activeSubs[activeSubs.length - 1].scrollIntoView({
+      block: "center",
+      inline: "nearest",
+      behavior: "instant",
+    });
+  }
 
   const dispatch = createEventDispatcher();
-
   function handleSeek(t: number) {
     dispatch("seek", { time: t });
   }
 </script>
 
 {#if cues}
-  <ul id="subtitle-list" style="">
-    {#each cues.toArray() as [id, cue]}
-      <!-- svelte-ignore a11y-positive-tabindex -->
-      <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-      <!-- svelte-ignore a11y-click-events-have-key-events -->
-      <!-- prettier-ignore -->
-      <li
-        id="subtitle-{id}"
-        class="subtitle"
-        tabindex="-1"
+  <ul id="subtitle-list">
+    {#each cues.toArray() as [id, cue], index}
+      <SubtitleItem
+        bind:startTime={cue.startTime}
+        bind:text={cue.text}
+        active={activeCueIds.has(id)}
         on:click={() => handleSeek(cue.startTime)}
-      >
-        <span style="user-select: none; font-size: small;">
-          {timeDisplay(cue.startTime)}:&nbsp;
-        </span>
-        {cue.text} 
-      </li>
+      />
     {/each}
   </ul>
 {/if}
@@ -40,9 +42,5 @@
     max-height: 100%;
     overflow: auto;
     list-style: none;
-  }
-
-  .subtitle {
-    cursor: pointer;
   }
 </style>
