074ca401ed4ed5ee498fd571639a444754c36662
sherwyn <sherwyn.sen@gmail.com>
2023-09-23 18:33:13 +0900

    2-1 - apply SubtitleItem component to SavedSubtitles

--- src/components/SavedSubtitles.svelte
+++ src/components/SavedSubtitles.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { createEventDispatcher } from "svelte";
   import { timeDisplay } from "../utils/utils";
+  import SubtitleItem from "./SubtitleItem.svelte";
 
   export function saveSubtitle(sub: VTTCue) {
     savedSubtitles.add(sub);
@@ -29,55 +30,16 @@
   let savedSubtitlesElement: HTMLUListElement;
 </script>
 
-<div>
-  <h3 id="title">Saved Subtitles</h3>
-  <ul id="subtitle-list" bind:this={savedSubtitlesElement}>
-    {#each Array.from(savedSubtitles) as sub}
-      <li style="display: flex; flex-direction: row">
-        <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <span on:click={() => handleSeek(sub.startTime)}>
-          <span style="user-select: none; font-size: small;">
-            {timeDisplay(sub.startTime)}:&nbsp;
-          </span>
-          <span>{sub.text}</span>
-        </span>
-        <!--<button class="delete-sub-button">X</button>-->
-      </li>
-    {/each}
-  </ul>
-</div>
+<hr />
+<ul id="subtitle-list" bind:this={savedSubtitlesElement}>
+  {#each Array.from(savedSubtitles) as sub}
+    <SubtitleItem
+      bind:startTime={sub.startTime}
+      bind:text={sub.text}
+      on:click={() => handleSeek(sub.startTime)}
+    />
+  {/each}
+</ul>
 
 <style>
-  div {
-    max-height: 100%;
-    display: flex;
-    flex-direction: column;
-  }
-
-  #title {
-    padding: 0px;
-    margin: 0px;
-  }
-
-  ul {
-    overflow: auto;
-    list-style: none;
-  }
-
-  li {
-    cursor: pointer;
-    color: #88aaff;
-    margin: 10px 0;
-  }
-
-  li:focus {
-    color: #aaff88;
-  }
-
-  /*
-  .delete-sub-button {
-    user-select: none;
-    padding: 0;
-  }
-  */
 </style>
